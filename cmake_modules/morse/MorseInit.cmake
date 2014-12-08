97b8d56f894525e09e95ac0a6686247b78aaa6dd
Florent Pruvost <florent.pruvost@inria.fr>
2014-12-08 15:36:59 +0000

    add personnal cmake modules: to color messages and find some headers (should be extend to libs)

--- cmake_modules/morse/MorseInit.cmake
+++ cmake_modules/morse/MorseInit.cmake
@@ -30,55 +30,14 @@
 # This include is required to check symbols of libs in the main CMakeLists.txt
 include(CheckFunctionExists)
 
+# To colorize messages
+include(ColorizeMessage)
+
+# To find headers and libs
+include(FindHeadersAndLibs)
+
 # Add the path where we handle our FindFOO.cmake to seek for liraries
 list(APPEND CMAKE_MODULE_PATH ${MORSE_CMAKE_MODULE_PATH}/find)
-
-# Set some colors
-if(NOT WIN32)
-    string(ASCII 27 Esc)
-    set(ColourReset "${Esc}[m")
-    set(ColourBold  "${Esc}[1m")
-    set(Red         "${Esc}[31m")
-    set(Green       "${Esc}[32m")
-    set(Yellow      "${Esc}[33m")
-    set(Blue        "${Esc}[34m")
-    set(Magenta     "${Esc}[35m")
-    set(Cyan        "${Esc}[36m")
-    set(White       "${Esc}[37m")
-    set(BoldRed     "${Esc}[1;31m")
-    set(BoldGreen   "${Esc}[1;32m")
-    set(BoldYellow  "${Esc}[1;33m")
-    set(BoldBlue    "${Esc}[1;34m")
-    set(BoldMagenta "${Esc}[1;35m")
-    set(BoldCyan    "${Esc}[1;36m")
-    set(BoldWhite   "${Esc}[1;37m")
-endif()
-
-# Colorize cmake messages during configure
-function(message)
-  list(GET ARGV 0 MessageType)
-  if(MessageType STREQUAL FATAL_ERROR OR MessageType STREQUAL SEND_ERROR)
-    list(REMOVE_AT ARGV 0)
-    string (REPLACE ";" " " ARGV_STR "${ARGV}")
-    _message(${MessageType} "${BoldRed}${ARGV_STR}${ColourReset}")
-  elseif(MessageType STREQUAL WARNING)
-    list(REMOVE_AT ARGV 0)
-    string (REPLACE ";" " " ARGV_STR "${ARGV}")
-    _message(${MessageType} "${BoldYellow}${ARGV_STR}${ColourReset}")
-  elseif(MessageType STREQUAL AUTHOR_WARNING)
-    list(REMOVE_AT ARGV 0)
-    string (REPLACE ";" " " ARGV_STR "${ARGV}")
-    _message(${MessageType} "${BoldCyan}${ARGV_STR}${ColourReset}")
-  elseif(MessageType STREQUAL STATUS)
-    list(REMOVE_AT ARGV 0)
-    string (REPLACE ";" " " ARGV_STR "${ARGV}")
-    _message(${MessageType} "${Green}${ARGV_STR}${ColourReset}")
-  else()
-    string (REPLACE ";" " " ARGV_STR "${ARGV}")
-    string (REPLACE "${Esc}[1 " "${Esc}[1;" ARGV_STR "${ARGV_STR}")
-    _message("${ARGV_STR}")
-  endif()
-endfunction()
 
 ##
 ## @end file MorseInit.cmake
