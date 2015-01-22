71e57ab2531e519195ab7622910a1d97a52cee94
Florent Pruvost <florent.pruvost@inria.fr>
2015-01-22 16:03:59 +0000

    add ctest MPI tests time_*potrf_tile

--- timing/CTestLists.cmake
+++ timing/CTestLists.cmake
@@ -17,13 +17,6 @@ if (CHAMELEON_USE_CUDA AND CUDA_FOUND)
    set( TEST_CATEGORIES ${TEST_CATEGORIES} shmgpu )
 endif()
 
-# if (CHAMELEON_USE_MPI AND MPI_C_FOUND)
-#    set( TEST_CATEGORIES ${TEST_CATEGORIES} mpi )
-#    if (CHAMELEON_USE_CUDA AND CUDA_FOUND)
-#       set( TEST_CATEGORIES ${TEST_CATEGORIES} mpigpu )
-#    endif()
-# endif()
-
 set(TESTLIST 
     gels
     gemm
@@ -52,4 +45,24 @@ foreach(cat ${TEST_CATEGORIES})
         endforeach()
     endforeach()
 endforeach()
+
+if (CHAMELEON_USE_MPI AND MPI_C_FOUND)
+    set( TEST_CATEGORIES mpi )
+    set( TEST_CMD_mpi    "--p=2 --n_range=20000:20000:1 --nb=320")
+    set( TEST_CMD_mpigpu "--p=2 --gpus=3")
+    #set( TEST_CATEGORIES ${TEST_CATEGORIES} mpi )
+    #if (CHAMELEON_USE_CUDA AND CUDA_FOUND)
+    #    set( TEST_CATEGORIES ${TEST_CATEGORIES} mpigpu )
+    #endif()
+    set(TESTLIST_MPI
+        potrf
+        )
+    foreach(cat ${TEST_CATEGORIES})
+        foreach(prec ${RP_CHAMELEON_PRECISIONS})
+            foreach(test ${TESTLIST_MPI})
+                add_test(time_${cat}_${prec}${test} ${MPI_CMD_${cat}} ./time_${prec}${test}_tile ${TEST_CMD_${cat}} --check --nowarmup)
+            endforeach()
+        endforeach()
+    endforeach()
+endif()
 
