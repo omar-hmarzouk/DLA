7d89707311905a2166f389bb334cd762b4dd1852
Florent Pruvost <florent.pruvost@inria.fr>
2016-05-25 11:31:41 +0000

    chameleon auxiliary.h: update proto declaration to avoid implicit declaration of function

--- control/auxiliary.h
+++ control/auxiliary.h
@@ -3,7 +3,7 @@
  * @copyright (c) 2009-2014 The University of Tennessee and The University
  *                          of Tennessee Research Foundation.
  *                          All rights reserved.
- * @copyright (c) 2012-2014 Inria. All rights reserved.
+ * @copyright (c) 2012-2016 Inria. All rights reserved.
  * @copyright (c) 2012-2014 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria, Univ. Bordeaux. All rights reserved.
  *
  **/
@@ -41,6 +41,13 @@ void morse_error        (const char *func_name, char* msg_text);
 void morse_fatal_error  (const char *func_name, char* msg_text);
 int  morse_rank         (MORSE_context_t *morse);
 int  morse_tune         (MORSE_enum func, int M, int N, int NRHS);
+
+/*******************************************************************************
+ *  API routines
+ **/
+int  MORSE_Version      (int *ver_major, int *ver_minor, int *ver_micro);
+int  MORSE_Element_Size (int type);
+int  MORSE_My_Mpi_Rank  (void);
 
 #ifdef __cplusplus
 }
