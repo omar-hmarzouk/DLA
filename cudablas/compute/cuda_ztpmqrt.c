a8954d6f03314e9c7d4bc7be49715fffd49e6281
Mathieu Faverge <mathieu.faverge@inria.fr>
2016-12-22 22:05:51 +0100

    Add missing cuda kernels

---
+++ cudablas/compute/cuda_ztpmqrt.c
@@ -0,0 +1,83 @@
+/**
+ *
+ * @copyright (c) 2009-2016 The University of Tennessee and The University
+ *                          of Tennessee Research Foundation.
+ *                          All rights reserved.
+ * @copyright (c) 2012-2016 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria,
+ *                          Univ. Bordeaux. All rights reserved.
+ *
+ **/
+
+/**
+ *
+ * @file cuda_ztpmqrt.c
+ *
+ *  MORSE cudablas kernel
+ *  MORSE is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley and Univ. of Colorado Denver,
+ *  and INRIA Bordeaux Sud-Ouest
+ *
+ * @author Florent Pruvost
+ * @date 2015-09-16
+ * @precisions normal z -> c d s
+ *
+ **/
+#include "cudablas/include/cudablas.h"
+#include "cudablas/include/cudablas_z.h"
+
+int
+CUDA_ztpmqrt( MORSE_enum side, MORSE_enum trans,
+              int M, int N, int K, int L, int IB,
+              const cuDoubleComplex *V, int LDV,
+              const cuDoubleComplex *T, int LDT,
+                    cuDoubleComplex *A, int LDA,
+                    cuDoubleComplex *B, int LDB,
+                    cuDoubleComplex *WORK,
+              CUBLAS_STREAM_PARAM )
+{
+    int m1, n1, ldwork, ldworkc, ws;
+
+    /* Check input arguments */
+    if ((side != MorseLeft) && (side != MorseRight)) {
+        cudablas_error(1, "Illegal value of side");
+        return -1;
+    }
+
+    if ( side == MorseLeft ) {
+        m1 = K;
+        n1 = N;
+        ldwork  = IB;
+        ldworkc = M;
+        ws = K * n1;
+    }
+    else {
+        m1 = M;
+        n1 = K;
+        ldwork  = m1;
+        ldworkc = IB;
+        ws = m1 * K;
+    }
+
+    /* TS case */
+    if (L == 0) {
+        CUDA_ztsmqr( side, trans, m1, n1, M, N, K, IB,
+                     A, LDA, B, LDB, V, LDV, T, LDT,
+                     WORK, ldwork, WORK + ws, ldworkc,
+                     CUBLAS_STREAM_VALUE );
+    }
+    /* TT case */
+    else  if( L == M ) {
+        cudablas_error(-6, "TTMQRT not available on GPU yet\n" );
+        return -6;
+        /* CUDA_zttmqr( side, trans, m1, n1, M, N, K, IB, */
+        /*              A, LDA, B, LDB, V, LDV, T, LDT, */
+        /*              WORK, ldwork ); */
+    }
+    else {
+        cudablas_error(-6, "TPMQRT not available on GPU yet\n" );
+        return -6;
+        //LAPACKE_ztpmqrt_work( LAPACK_COL_MAJOR, M, N, K, L, IB, V, LDV, T, LDT, A, LDA, B, LDB, WORK );
+    }
+
+    return MORSE_SUCCESS;
+}
