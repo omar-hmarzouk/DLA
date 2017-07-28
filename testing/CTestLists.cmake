5d465e254f4f56d55755ba5133fa4aa085b98a08
Mathieu Faverge <mathieu.faverge@inria.fr>
2017-07-28 16:59:33 +0200

    Set policy to avoid cmake warning

--- testing/CTestLists.cmake
+++ testing/CTestLists.cmake
@@ -1,3 +1,5 @@
+cmake_policy(SET CMP0054 NEW)
+
 #
 # Check testing/
 #
@@ -47,8 +49,8 @@ foreach(cat  ${TEST_CATEGORIES})
          add_test(test_${cat}_${prec}gels_hlq_fibonacci ./${prec}${TEST_CMD_${cat}} GELS_HQR 1000 1500 1000 10 1000 4 -1 2 -1 0)
          add_test(test_${cat}_${prec}gels_hlq_binary    ./${prec}${TEST_CMD_${cat}} GELS_HQR 1000 1500 1000 10 1000 4 -1 3 -1 0)
 
-         add_test(test_${cat}_${prec}gels_rq_systolic      ./${prec}${TEST_CMD_${cat}} GELS_SYSTOLIC 1000 1000 1000 10 1000 3 2)
-         add_test(test_${cat}_${prec}gels_lq_systolic      ./${prec}${TEST_CMD_${cat}} GELS_SYSTOLIC 1000 1500 1000 10 1000 3 2)
+         add_test(test_${cat}_${prec}gels_rq_systolic   ./${prec}${TEST_CMD_${cat}} GELS_SYSTOLIC 1000 1000 1000 10 1000 3 2)
+         add_test(test_${cat}_${prec}gels_lq_systolic   ./${prec}${TEST_CMD_${cat}} GELS_SYSTOLIC 1000 1500 1000 10 1000 3 2)
      endif()
 
   endforeach()
