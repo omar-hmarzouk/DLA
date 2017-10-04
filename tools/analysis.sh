9a5d3474fc1f444735ae5c9d7d1be33055e42366
Florent Pruvost <florent.pruvost@inria.fr>
2017-10-04 16:39:42 +0200

    test new pipeline on starpu build

--- tools/analysis.sh
+++ tools/analysis.sh
@@ -4,19 +4,20 @@
 # We consider to be in Chameleon's source code root
 
 # build with proper options
-mkdir -p build
-cd build
-rm * -rf
-cmake .. -DCHAMELEON_USE_MPI=ON -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_VERBOSE_MAKEFILE=ON -DMORSE_ENABLE_WARNING=ON -DMORSE_ENABLE_COVERAGE=ON
-make -j5 | tee ../chameleon-build.log
+#mkdir -p build
+#cd build
+#rm * -rf
+#cmake .. -DCHAMELEON_USE_MPI=ON -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_VERBOSE_MAKEFILE=ON -DMORSE_ENABLE_WARNING=ON -DMORSE_ENABLE_COVERAGE=ON
+#make -j5 | tee ../chameleon-build.log
 
 # run tests
-STARPU_SILENT=1 ctest --no-compress-output || /usr/bin/true
+#STARPU_SILENT=1 ctest --no-compress-output || /usr/bin/true
 
 # capture coverage
-lcov --directory . --capture --output-file ../chameleon.lcov
-cd ..
-genhtml -o coverage chameleon.lcov
+#lcov --directory . --capture --output-file ../chameleon.lcov
+lcov --directory build --capture --output-file chameleon.lcov
+#cd ..
+#genhtml -o coverage chameleon.lcov
 lcov_cobertura.py chameleon.lcov --output chameleon-coverage.xml
 
 # filter sources:
@@ -28,7 +29,8 @@ lcov_cobertura.py chameleon.lcov --output chameleon-coverage.xml
 # Undefine this because not relevant in our configuration
 export UNDEFINITIONS="-UCHAMELEON_USE_OPENCL -UWIN32 -UWIN64 -U_MSC_EXTENSIONS -U_MSC_VER -U__SUNPRO_C -U__SUNPRO_CC -U__sun -Usun -U__cplusplus"
 # run cppcheck analysis
-cppcheck -v -f --language=c --platform=unix64 --enable=all --xml --xml-version=2 --suppress=missingIncludeSystem ${UNDEFINITIONS} --file-list=./filelist.txt 2> chameleon-cppcheck.xml
+#cppcheck -v -f --language=c --platform=unix64 --enable=all --xml --xml-version=2 --suppress=missingIncludeSystem ${UNDEFINITIONS} --file-list=./filelist.txt 2> chameleon-cppcheck.xml
+cppcheck -v -f --language=c --platform=unix64 --enable=all --xml --xml-version=2 --suppress=missingIncludeSystem ${UNDEFINITIONS} `cat filelist.txt` 2> chameleon-cppcheck.xml
 # run rats analysis
 rats -w 3 --xml  `cat filelist.txt` > chameleon-rats.xml
 
