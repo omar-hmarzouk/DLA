7be89889ddba808a22ee6b1853ee175e86885ec8
Florent Pruvost <florent.pruvost@inria.fr>
2017-12-06 11:36:34 +0100

    add a cmake script to initiate cache for gitlab-ci

---
+++ cmake_modules/gitlab-ci-initial-cache.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_INSTALL_PREFIX "$PWD/install" CACHE PATH "")
+
+set(CMAKE_VERBOSE_MAKEFILE "ON" CACHE BOOL "")
+
+option(MORSE_ENABLE_WARNING       "Enable warning messages" ON)
+option(MORSE_ENABLE_COVERAGE      "Enable flags for coverage test" ON)
