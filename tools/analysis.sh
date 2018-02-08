ff3d51f04024c977818c0d81231e33fd7e30c0e6
Mathieu Faverge <mathieu.faverge@inria.fr>
2018-02-08 18:40:04 +0100

    Update sonarqube

--- tools/analysis.sh
+++ tools/analysis.sh
@@ -14,32 +14,36 @@
 # Undefine this because not relevant in our configuration
 export UNDEFINITIONS="-UCHAMELEON_USE_OPENCL -UWIN32 -UWIN64 -U_MSC_EXTENSIONS -U_MSC_VER -U__SUNPRO_C -U__SUNPRO_CC -U__sun -Usun -U__cplusplus"
 # run cppcheck analysis
-cppcheck -v -f --language=c --platform=unix64 --enable=all --xml --xml-version=2 --suppress=missingIncludeSystem ${UNDEFINITIONS} --file-list=./filelist.txt 2> chameleon_cppcheck.xml
+cppcheck -v -f --language=c --platform=unix64 --enable=all --xml --xml-version=2 --suppress=missingInclude ${UNDEFINITIONS} --file-list=./filelist.txt 2> chameleon_cppcheck.xml
 # run rats analysis
 rats -w 3 --xml  `cat filelist.txt` > chameleon_rats.xml
 
 # create the sonarqube config file
 cat > sonar-project.properties << EOF
-sonar.host.url=https://hpclib-sed.bordeaux.inria.fr/sonarqube-dev
+sonar.host.url=https://sonarqube.bordeaux.inria.fr/
+sonar.login=$SONARQUBE_LOGIN
+
 sonar.links.homepage=https://gitlab.inria.fr/solverstack/chameleon
+sonar.links.scm=https://gitlab.inria.fr/solverstack/chameleon.git
 sonar.links.ci=https://gitlab.inria.fr/solverstack/chameleon/pipelines
-sonar.links.scm=https://gitlab.inria.fr/solverstack/chameleon/
-sonar.projectKey=chameleon
-sonar.projectName=Chameleon
+sonar.links.issue=https://gitlab.inria.fr/solverstack/chameleon/issues
+
+sonar.projectKey=hiepacs:chameleon:gitlab:master
 sonar.projectDescription=Dense linear algebra subroutines for heterogeneous and distributed architectures
 sonar.projectVersion=master
-sonar.language=c++
+
+sonar.language=c
 sonar.sources=build, compute, control, coreblas, example, include, runtime, testing, timing
 sonar.inclusions=`cat filelist.txt | xargs echo | sed 's/ /, /g'`
 sonar.sourceEncoding=UTF-8
-sonar.cxx.compiler.charset=UTF-8
-sonar.cxx.compiler.parser=GCC
-sonar.cxx.compiler.regex=^(.*):(\\d+):\\d+: warning: (.*)\\[(.*)\\]$
-sonar.cxx.compiler.reportPath=chameleon_starpu.log, chameleon_starpu_simgrid.log, chameleon_quark.log
-sonar.cxx.coverage.reportPath=chameleon_coverage.xml
-sonar.cxx.cppcheck.reportPath=chameleon_cppcheck.xml
-sonar.cxx.rats.reportPath=chameleon_rats.xml
+sonar.c.compiler.charset=UTF-8
+sonar.c.compiler.parser=GCC
+sonar.c.compiler.regex=^(.*):(\\d+):\\d+: warning: (.*)\\[(.*)\\]$
+sonar.c.compiler.reportPath=chameleon_starpu.log, chameleon_starpu_simgrid.log, chameleon_quark.log
+sonar.c.coverage.reportPath=chameleon_coverage.xml
+sonar.c.cppcheck.reportPath=chameleon_cppcheck.xml
+sonar.c.rats.reportPath=chameleon_rats.xml
 EOF
 
 # run sonar analysis + publish on sonarqube-dev
-sonar-scanner -X -Dsonar.login=077ef775a8b4fe9ba722497ef0511ca6dcfb3fcd > sonar.log
+sonar-scanner -X > sonar.log
